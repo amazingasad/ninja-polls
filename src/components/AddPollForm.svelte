f5deb2126c55be2e8f6d553990329dda2bc0ef8e
amazingasad <asadme631@gmail.com>
2021-12-07 15:32:24 +0600

    Added  Poll form validation

--- src/components/AddPollForm.svelte
+++ src/components/AddPollForm.svelte
@@ -1,7 +1,29 @@
 <script>
     import Button from "../shared/Button.svelte"
     let data = {question: "", answerA: "", answerB: ""};
+    let errors = {question: "", answerA: "", answerB: ""};
+    let valid = false;
     const handleSubmit = () => {
+        // question validation
+        valid = true;
+        if (data.question.trim().length < 5) {
+            errors.question = "Question must have at least 5 characters";
+            valid = false;
+        }else{
+            errors.question = "";
+        }
+        if (data.answerA.trim().length <1 ) {
+            errors.answerA = "Answer A can not be empty";
+            valid = false;
+        }else{
+            errors.answerA = "";
+        }
+        if (data.answerB.trim().length < 1) {
+            errors.answerB = "Answer B can not be empty";
+            valid = false;
+        }else{
+            errors.answerB = "";
+        }
         console.log(data)
     }
 </script>
@@ -9,10 +31,13 @@
     <div class="form-feild">
         <label for="question">Question :</label>
         <input type="text" id="question" bind:value="{data.question}">
+        <p>{errors.question}</p>
         <label for="answer-a">Answer A :</label>
         <input type="text" id="answer-a" bind:value="{data.answerA}">
+        <p>{errors.answerA}</p>
         <label for="answer-b">Answer B :</label>
         <input type="text" id="answer-b" bind:value="{data.answerB}">
+        <p>{errors.answerB}</p>
         <Button type="secondary">Add Poll</Button>
     </div>
 </form>
@@ -22,6 +47,12 @@
         margin: 0 auto;
         text-align: center;
     }
+    .form-feild p{
+        color: crimson;
+        font-size: 12px;
+        font-weight: bold;
+        margin-top: -5px;
+    }
     label{
         text-align: left;
         margin: 10px 0;
@@ -29,8 +60,5 @@
     input{
         border-radius: 6px;
         width: 100%;
-    }
-    button{
-        padding: 10px 25px;
     }
 </style>
