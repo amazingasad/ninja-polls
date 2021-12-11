c6089b7fd7ceb81ab36878c1945caea7bc940b10
amazingasad <asadme631@gmail.com>
2021-12-11 18:34:37 +0600

    Added Dynamic Percent Bar

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -4,6 +4,8 @@
     import Card from "../shared/Card.svelte"
     export let poll;
     $:totalVote = poll.VoteA + poll.VoteB;
+    $:percentA = Math.floor(100 / totalVote * poll.VoteA)
+    $:percentB = Math.floor(100 / totalVote * poll.VoteB)
     const handleVote = (option, id) =>{
         dispatch("vote", {option, id});
     };
@@ -13,11 +15,11 @@
         <h3> {poll.question} </h3>
         <p>Total Votes : {totalVote}</p>
         <div class="answer" on:click={() => handleVote("a", poll.id)}>
-            <div class="percent percent-a"></div>
+            <div class="percent percent-a" style="width: {percentA}%;"></div>
             <span>{poll.answerA} ({poll.VoteA})</span>
         </div>
         <div class="answer" on:click={() => handleVote("b", poll.id)}>
-            <div class="percent percent-b"></div>
+            <div class="percent percent-b" style="width: {percentB}%;"></div>
             <span>{poll.answerB} ({poll.VoteB})</span>
         </div>
     </div>
@@ -41,6 +43,19 @@
     }
     .answer:hover{
         opacity: 0.6;
+    }
+    .percent{
+        height: 100%;
+        position: absolute;
+        box-sizing: border-box;
+    }
+    .percent-a{
+        background-color: rgba(217, 27, 66, 0.4);
+        border-left: 3px solid #d91b42;
+    }
+    .percent-b{
+        border-left: 3px solid #45c496;
+        background-color: rgba(69, 196, 150, 0.4);
     }
     span{
         display: inline-block;
